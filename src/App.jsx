16cf36f840f9124555fdd10f5d2e6f3729a6522b
CherokiDev <miguelangelmorato84@gmail.com>
2024-05-24 14:38:49 +0200

    added styles to login component

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,58 @@ import {
   Navigate,
 } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
-import { ToastContainer } from "react-toastify";
+// import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ResetPassword from "./components/ResetPassword";
+import { SnackbarProvider } from "notistack";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Box, CircularProgress } from "@mui/material";
 
 const Login = lazy(() => import("./components/Login"));
 const Signup = lazy(() => import("./components/Signup"));
 const Home = lazy(() => import("./components/Home"));
 
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Roboto Slab', serif",
+  },
+});
+
 const App = () => {
   return (
-    <Router>
-      <ToastContainer />
-      <Suspense fallback={<div>Loading con Suspense...</div>}>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/reset/:token" element={<ResetPassword />} />
-          <Route path="/home" element={<ProtectedRoute element={<Home />} />} />
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="*" element={<ProtectedRoute element={<Home />} />} />
-        </Routes>
-      </Suspense>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <SnackbarProvider
+          maxSnack={3}
+          anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        >
+          <Suspense
+            fallback={
+              <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                height="100vh"
+              >
+                <CircularProgress />
+              </Box>
+            }
+          >
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/reset/:token" element={<ResetPassword />} />
+              <Route
+                path="/home"
+                element={<ProtectedRoute element={<Home />} />}
+              />
+              <Route path="/" element={<Navigate to="/login" />} />
+              <Route path="*" element={<ProtectedRoute element={<Home />} />} />
+            </Routes>
+          </Suspense>
+        </SnackbarProvider>
+      </Router>
+    </ThemeProvider>
   );
 };
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,30 @@
 import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
+import {
+  Button,
+  TextField,
+  Container,
+  Typography,
+  Fade,
+  Paper,
+  Divider,
+} from "@mui/material";
+import { useSnackbar } from "notistack";
 
 const Login = () => {
   const [identifier, setIdentifier] = useState("");
   const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
+  const [showForgotPassword, setShowForgotPassword] = useState(false);
+  const [forgotPasswordEmail, setForgotPasswordEmail] = useState("");
+  const [loginAttempts, setLoginAttempts] = useState(0);
   const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
 
   useEffect(() => {
     return () => {
       setIdentifier("");
       setPassword("");
-      setEmail("");
     };
   }, []);
 
@@ -36,16 +47,22 @@ const Login = () => {
         localStorage.setItem("id", response.data.id);
         localStorage.setItem("role", response.data.role);
         navigate("/home");
-        toast("Bienvenido!");
+        enqueueSnackbar("Bienvenido!", { variant: "success" });
       } catch (error) {
+        setLoginAttempts((prevAttempts) => prevAttempts + 1);
+        if (loginAttempts >= 1) {
+          setShowForgotPassword(true);
+        }
         if (error.response && error.response.data.message) {
-          toast(error.response.data.message, { type: "error" });
+          enqueueSnackbar(error.response.data.message, { variant: "error" });
         } else {
-          toast("Ocurrió un error inesperado", { type: "error" });
+          enqueueSnackbar("Ocurrió un error al iniciar sesión", {
+            variant: "error",
+          });
         }
       }
     },
-    [identifier, password, navigate]
+    [identifier, password, navigate, enqueueSnackbar, loginAttempts]
   );
 
   const handleSignup = useCallback(() => {
@@ -56,49 +73,115 @@ const Login = () => {
     e.preventDefault();
     try {
       await axios.post(`${import.meta.env.VITE_API_URL}/players/forgot`, {
-        email,
+        email: forgotPasswordEmail,
       });
-      toast("Correo electrónico de recuperación enviado.");
+      enqueueSnackbar("Correo electrónico de recuperación enviado.", {
+        variant: "success",
+      });
     } catch (error) {
-      toast("Error al enviar el correo electrónico de recuperación.", {
-        type: "error",
-      });
+      enqueueSnackbar(error.response.data.message, { variant: "error" });
     }
   };
 
+  const duration = 2000;
+
   return (
-    <>
+    <Container component="main" maxWidth="xs">
+      <Paper elevation={3} style={{ padding: "20px", marginBottom: "20px" }}>
+        <Typography variant="h4" align="center">
+          Padel Match
+        </Typography>
+      </Paper>
+      <Typography component="h1" variant="h5">
+        Iniciar sesión
+      </Typography>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="identifier"
+          label="Email o nombre de usuario"
+          name="identifier"
+          autoComplete="identifier"
+          autoFocus
           value={identifier}
           onChange={(e) => setIdentifier(e.target.value)}
-          placeholder="Email o nombre de usuario"
-          required
+          style={{ marginBottom: "0px" }}
         />
-        <input
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Contraseña"
           type="password"
+          id="password"
+          autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          required
         />
-        <button type="submit">Login</button>
-        <button type="button" onClick={handleSignup}>
-          Sign Up
-        </button>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          style={{ marginBottom: "10px" }}
+        >
+          Iniciar sesión
+        </Button>
+
+        <Divider variant="middle" style={{ margin: "20px 0" }} />
+
+        <Typography variant="body1" style={{ marginTop: "10px" }}>
+          ¿No tienes cuenta?
+        </Typography>
+
+        <Button
+          onClick={handleSignup}
+          fullWidth
+          variant="outlined"
+          color="primary"
+          style={{ backgroundColor: "#f5f5f5" }}
+        >
+          Regístrarse
+        </Button>
       </form>
-      <form onSubmit={handleForgotPassword}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email for password reset"
-          required
-        />
-        <button type="submit">Forgot Password</button>
-      </form>
-    </>
+      <Fade
+        in={showForgotPassword}
+        timeout={duration}
+        style={{ marginTop: "50px" }}
+      >
+        <form onSubmit={handleForgotPassword}>
+          <Typography variant="body1" style={{ marginTop: "10px" }}>
+            ¿Olvidaste tu contraseña?
+          </Typography>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="forgotPasswordEmail"
+            label="Email para recuperar contraseña"
+            name="forgotPasswordEmail"
+            autoComplete="email"
+            value={forgotPasswordEmail}
+            onChange={(e) => setForgotPasswordEmail(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="outlined"
+            color="primary"
+            style={{ backgroundColor: "#F4DDA7" }}
+          >
+            Enviar correo de recuperación
+          </Button>
+        </form>
+      </Fade>
+    </Container>
   );
 };
 
