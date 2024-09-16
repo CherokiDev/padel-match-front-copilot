ec13026c444e2e7293dd049de3712e1905ac9f41
CherokiDev <miguelangelmorato84@gmail.com>
2024-09-16 18:53:08 +0200

    Refactor redux store devTools option to use environment variable

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import profileSlice from "./profileSlice";
 import schedulesSlice from "./schedulesSlice";
 
+const isDevelompent = import.meta.env.MODE === "development";
+
 export const store = configureStore({
   reducer: {
     profile: profileSlice,
     schedules: schedulesSlice,
   },
-  devTools: true,
+  devTools: isDevelompent,
 });
