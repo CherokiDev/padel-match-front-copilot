f9c5e004690d81a2e4cf39d14ad376af81519fab
CherokiDev <miguelangelmorato84@gmail.com>
2024-11-24 17:31:02 +0100

    Refactor LoadingScreen component: Simplify loading logic and update UI with MUI CircularProgress

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,37 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import PropTypes from "prop-types";
-import "./LoadingScreen.css";
+import { CircularProgress, Box } from "@mui/material";
 
 const LoadingScreen = ({ onLoadingComplete }) => {
-  const [progress, setProgress] = useState(0);
-
   useEffect(() => {
-    const interval = setInterval(() => {
-      setProgress((oldProgress) => {
-        if (oldProgress === 100) {
-          clearInterval(interval);
-          onLoadingComplete();
-          return 100;
-        }
-        return Math.min(oldProgress + 10, 100);
-      });
-    }, 200); // Se puede ajustar el tiempo de carga
-
-    return () => {
-      clearInterval(interval);
-    };
+    // Simular la finalización de la carga cuando sea necesario
+    onLoadingComplete();
   }, [onLoadingComplete]);
 
   return (
-    <div className="loading-screen">
-      <div className="loading-background"></div>
-      <div className="loading-bar">
-        <div
-          className="loading-progress"
-          style={{ width: `${progress}%` }}
-        ></div>
-      </div>
-    </div>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <CircularProgress />
+    </Box>
   );
 };
 
